f78a619694f6f24145bca3e0ae83924b21869bad
Shayne Shelton <123333803+sshelton1@users.noreply.github.com>
2023-05-02 01:41:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,12 +16,14 @@ def reachable(graph, start_node):
     """
     result = set([start_node])
     frontier = set([start_node])
+    visited = set()
     while len(frontier) != 0:
         ###TODO
         current = frontier.pop()
+        visited.add(current)
         adjacent = graph[current]
         frontier.update(adjacent)
-        frontier.difference_update(result)
+        frontier.difference_update(visited)
         result.update(adjacent)
     result = sorted(list(result))
     return result
@@ -33,13 +35,18 @@ def test_reachable():
     graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B'), ('E', 'F'), ('F', 'G')])
     assert sorted(reachable(graph, 'A')) == ['A', 'B', 'C', 'D']
     assert sorted(reachable(graph, 'E')) == ['E', 'F', 'G']
-
-
-
+    
 
 def connected(graph):
     ### TODO
-    pass
+    n =  len(list(graph.keys()))
+    checked = set()
+    for node in graph.keys():
+        if node not in checked:
+            checked.union(graph[node])
+            if len(reachable(graph, node)) != n:
+                return False
+    return True
 
 def test_connected():
     graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B')])
@@ -55,7 +62,21 @@ def n_components(graph):
       the number of connected components in an undirected graph
     """
     ### TODO
-    pass
+    n =  1
+    checked = []
+    for node in graph.keys():
+        if checked == []:
+            checked.append(reachable(graph, node))
+        else:
+            i = 0
+            while i < len(checked):
+                if node in checked[i]:
+                    break
+                i += 1
+            if i == len(checked):
+                checked.append(reachable(graph, node))
+            n += 1
+    return n
 
 def test_n_components():
     graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B')])
